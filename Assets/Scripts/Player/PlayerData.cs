66e1ad88a932f39e285dbbe39fddef102ec7845b
Cloudy <crosska111@gmail.com>
2018-11-06 23:54:56 +0100

    network behaviour for player data

--- Assets/Scripts/Player/PlayerData.cs
+++ Assets/Scripts/Player/PlayerData.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerData : MonoBehaviour {
-    public float Health      = 100f;
-    public float HealthMax   = 100f;
+public class PlayerData : NetworkBehaviour {
+    [SyncVar]
+    public float Health = 100f;
+
+    [SyncVar]
+    public float HealthMax = 100f;
+
+    [SyncVar]
     public float DamageAdder = 0f;
-    public float Speed       = 2f;
+
+    [SyncVar]
+    public float Speed = 2f;
 
     // Use this for initialization
     void Start() {
